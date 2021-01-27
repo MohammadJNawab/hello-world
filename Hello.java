24c6c128301aa9d717120178cf253581fc90166e
MohammadJNawab <78092572+MohammadJNawab@users.noreply.github.com>
2021-01-27 10:17:52 -0500

    Hello.java
    First lab completed to download class-related applications and to ensure students can create then run java programs.

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************************************************
+ * Lab 1
+ *************************************************************************
+ * Mohammad Nawab
+ * 1/26/2021
+ * CMSC-255-003
+ ************************************************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	System.out.println("Mohammad Nawab");
+	System.out.println("Lab 1");
+	System.out.println("Make sure class related applications are downloaded, then create and run a Java program");
+	System.out.println("1/26/2021");
+	System.out.println("CMSC-255-003");
+	System.out.println();
+	}
+}
